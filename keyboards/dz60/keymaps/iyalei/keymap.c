75a3ea1fb4dff10cfcda7536a2f0969c05825af6
anyalir <anyarudo@gmail.com>
2021-04-16 23:50:25 +0200

    save keyboard layouts

--- keyboards/dz60/keymaps/iyalei/keymap.c
+++ keyboards/dz60/keymaps/iyalei/keymap.c
@@ -6,8 +6,11 @@
 // entirely and just use numbers.
 enum layer_names {
   _BL,
-  _FL
+  _FL,
+  _NL
 };
+
+#define ESC_FL LT(_FL, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /* Keymap _BL: (Base Layer) Default Layer
@@ -24,12 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `-----------------------------------------------------------'
     */
     [_BL] = LAYOUT_60_ansi(
-        KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-        KC_ESC,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,                   KC_RSFT, \
-        KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                             KC_RALT, MO(_FL), KC_RGUI, KC_RCTL
-    ),
+        KC_GRV,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINS,KC_EQL,KC_BSPC,\
+        KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_LBRC,KC_RBRC,KC_BSLS,\
+        LT(_FL,KC_ESC),KC_A,KC_S,KC_D,KC_F,KC_G,KC_H,KC_J,KC_K,KC_L,KC_SCLN,KC_QUOT,KC_ENT,\
+        KC_LSFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_N,KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_RSFT,\
+        KC_LCTL,KC_LGUI,KC_LALT,KC_SPC,KC_RALT,MO(_FL),TG(_NL),KC_APP
+      ),
 
    /* Keymap _FL: Function Layer
     * ,-----------------------------------------------------------.
@@ -46,10 +49,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * hjkl for arrows because vim
     */
     [_FL] = LAYOUT_60_ansi(
-        RESET, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-        KC_TRNS, KC_HOME, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC,  BL_INC,  BL_TOGG, \
-        KC_TRNS, KC_END,  KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, KC_MPRV, KC_MNXT,          KC_MPLY, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_VOLD, KC_VOLU, KC_MUTE,                   KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+      RESET,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,KC_DEL,\
+      RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,RGB_SPI,RGB_MOD,KC_HOME,KC_PGDN,KC_PGUP,KC_END,BL_TOGG,BL_DEC,BL_INC,KC_PSCR,\
+      _______,RGB_HUD,RGB_SAD,RGB_VAD,RGB_SPD,RGB_RMOD,KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,BL_STEP,BL_BRTG,_______,\
+      _______,KC_BRID,KC_BRIU,_______,_______,KC_MPLY,KC_MRWD,KC_VOLD,KC_VOLU,KC_MFFD,KC_MSTP,_______,\
+      _______,_______,_______,_______,_______,_______,_______,_______
     ),
+
+    [_NL] = LAYOUT_60_ansi(
+      _______,_______,_______,_______,_______,_______,_______,_______,KC_NLCK,KC_PSLS,KC_PAST,KC_PMNS,_______,_______,\
+      _______,_______,_______,_______,_______,_______,_______,_______,KC_P7,KC_P8,KC_P9,KC_PPLS,_______,_______,\
+      _______,_______,_______,_______,_______,_______,_______,_______,KC_P4,KC_P5,KC_P6,KC_PCMM,KC_PENT,\
+      _______,NK_ON,NK_OFF,_______,_______,_______,_______,_______,KC_P1,KC_P2,KC_P3,KC_PEQL,\
+      _______,_______,_______,_______,KC_P0,KC_P0,KC_PDOT,_______
+    )
 };
---
+++ keyboards/yd60mq/keymaps/iyalei/rules.mk
@@ -0,0 +1,5 @@
+# Build Options
+#   comment out to disable the options.
+#
+BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes
